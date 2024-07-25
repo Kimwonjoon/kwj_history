ce80eed816bc51c9af5b1ba8076d834b41e70bbe
Kimwonjoon <kimpass189@gmail.com>
2024-07-25 17:40:23 +0900

    add tabulate

--- src/kwj_history/cli.py
+++ src/kwj_history/cli.py
@@ -1,6 +1,7 @@
-#import pandas as pd
+import pandas as pd
 import sys
 import argparse
+from tabulate import tabulate
 def cnt(q):
     df = read_parquet()
     df = pd.read_parquet('~/tmp/history.parquet')
@@ -30,16 +31,17 @@ def day_df(d):
 def double_df(num,d):
     df = pd.read_parquet('~/data/parquet')
     df2 = df[df['dt'] == d].sort_values(by='cnt', ascending = False).head(num)
-    df2 = df2.drop(columns = ['dt']).to_string(index=False)
+    df2 = df2.drop(columns = ['dt'])
     return df2
 
 def query():
-    import pandas as pd
+#    import pandas as pd
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-s", "--search", help="hist -s <ls>",type=str, action='store')
     parser.add_argument("-t", "--top", help="hist -t <int>",type=int,action='store')
     parser.add_argument("-d", "--day", help="hist -d <YYYY-MM-DD>", type=str,action='store')
+    parser.add_argument("-p", "--pretty", help="hist -p",action='store_true')
 
     args = parser.parse_args()
     if args.search:
@@ -48,7 +50,10 @@ def query():
         print(f'{q} 사용 횟수는 {i}회 입니다.')
     elif args.top and args.day:
         r = double_df(args.top, args.day)
-        print(r)
+        if args.pretty:
+            print(tabulate(r, tablefmt="pipe", showindex="always", headers="keys"))
+        else:
+            print(r)
     elif args.top:
         r = top_df(args.top)
         print(r)
